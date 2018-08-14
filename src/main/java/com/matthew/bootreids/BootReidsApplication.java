a7d40501e43e2d00a01308d15e1604d699dfb9dc
Matthew <matthew_java@163.com>
2018-08-14 16:28:22 +0800

    Initial commit

---
+++ src/main/java/com/matthew/bootreids/BootReidsApplication.java
@@ -0,0 +1,12 @@
+package com.matthew.bootreids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootReidsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootReidsApplication.class, args);
+    }
+}
